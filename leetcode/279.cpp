90a2ac94763d05cb3cbbd2dd12ffb7d4143b1f24
hlyyy <2458839534@qq.com>
2022-05-13 13:27:54 +0800

    leetcode++

---
+++ leetcode/279.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        int dp[n+1];
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i-1] + 1;
+            for (int j = 1; i-j*j >= 0; j++)
+                dp[i] = min(dp[i],dp[i-j*j]+1);
+        }
+        return dp[n];
+    }
+};
